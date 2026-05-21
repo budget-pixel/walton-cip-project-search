781e2011e809dd9efc390bfd9bad03c2818e151e
budget-pixel <peydevon@gmail.com>
2026-05-21 12:36:11 -0500

    Update app.jsd

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const app = document.getElementById("app");
-
-app.innerHTML = `
-  <section style="padding:40px;font-family:Arial, Helvetica, sans-serif;">
-    <h1 style="color:#006231;">Walton County CIP Project Search</h1>
-    <p>Loaded ${wcProjects.length} project(s) from projects.js.</p>
-  </section>
-`;
+document.getElementById("app").innerHTML = "APP JS IS RUNNING";
